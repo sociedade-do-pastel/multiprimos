50de00d25ff6a0ecb54a502a7ea2974a4adbd39e
Pedro Barbosa <contato.pfmb@gmail.com>
2021-09-28 19:56:59 -0300

    Adicionada conexão única para envio dos dados

--- proc1.py
+++ proc1.py
@@ -1,22 +1,28 @@
 import socket, sys
 import pandas as pd
 
-def request_key(initial_value, n, sock):
-    sock.connect(("127.0.0.1", 65202))
-    sock.sendall(bytes((f'{initial_value} {n}').encode('UTF-8')))
-    answer = sock.recv(1024).decode()
-    print(f'Resposta do servidor: {answer}')
-    # print(f'{initial_value} {n}')
+def request_key(values, sock):
+    counter = 0
+
+    for initial_value, n in values:
+        sock.sendall(bytes((f'{initial_value} {n}').encode('UTF-8')))
+        answer = sock.recv(1024).decode()
+        print(f'Resposta do servidor: {answer}')
+        if answer != "d": counter += 1
+
+    print(f'{counter} chaves foram geradas!')
+    sock.close()
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('File name must be called as first parameter')
+        print('File name (with extension) must be called as first parameter')
         exit()
 
     file_name = sys.argv[1]
 
-    df = pd.read_excel(file_name, header=None)
+    df = pd.read_csv(file_name, header=None, delimiter=",")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        for initial_value, n in zip(df[0], df[1]):
-            request_key(initial_value, n, sock)
+        sock.connect(("127.0.0.1", 20001))
+        iterator = zip(df[0], df[1])
+        request_key(iterator, sock)
